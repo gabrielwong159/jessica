c459c882e3507055babd02f7e3c47b84308e3f2c
Gabriel Wong <gabrielwong159@gmail.com>
2019-04-05 21:17:41 +0800

    Add example scripts on Niryo arm movement

--- examples/gpio.py
+++ examples/gpio.py
@@ -2,19 +2,19 @@
 import time
 from niryo_one_python_api.niryo_one_api import *
 
-SENSOR = GPIO_1A
-LED = GPIO_2A
+SENSOR_PIN = GPIO_1A
+LED_PIN = GPIO_2A
 
 n = NiryoOne()
-
+n.pin_mode(SENSOR, PIN_MODE_INPUT)
+n.pin_mode(LED, PIN_MODE_OUTPUT)
 
 if __name__ == '__main__':
-    n.pin_mode(SENSOR, 1)
-#    n.pin_mode(LED, 0)
 
     while True:
-        val = n.digital_read(SENSOR)
-#        n.digital_write(LED, val)
-        time.sleep(0.1)
+        val = n.digital_read(SENSOR_PIN)
         print(val)
+
+        n.digital_write(LED_PIN, val)
+        time.sleep(0.1)
 
